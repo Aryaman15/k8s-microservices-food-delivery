7f9673d073ab1a9d7ffef67b90e93d82185484d6
Naramala Siva Vekata Pavan <sivavenkatapavan@gmail.com>
2025-07-06 15:21:46 +0530

    feat(order-service): temporarily disable authorization

--- backend/order-service/routes/orderRoutes.js
+++ backend/order-service/routes/orderRoutes.js
@@ -7,10 +7,12 @@ const authMiddleware = require("../middlewares/authMiddleware");
 router
   .route("/orders")
   .get(/*authMiddleware.isAdmin,*/ wrapAsync(orderController.allOrders))
-  .post(authMiddleware.protect, wrapAsync(orderController.newOrder));
+  .post(/*authMiddleware.protect,*/ wrapAsync(orderController.newOrder));
 router
   .route("/orders/:id/status")
-  .get(authMiddleware.protect, wrapAsync(orderController.getOrderStatus))
-  .patch(authMiddleware.protect, wrapAsync(orderController.updateOrderStatus));
+  .get(/*authMiddleware.protect,*/ wrapAsync(orderController.getOrderStatus))
+  .patch(
+    /*authMiddleware.protect,*/ wrapAsync(orderController.updateOrderStatus)
+  );
 
 module.exports = router;

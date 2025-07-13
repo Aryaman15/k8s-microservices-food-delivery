28fb873016e1a24fdb788dbbf698c4f5279ba68e
Naramala Siva Vekata Pavan <sivavenkatapavan@gmail.com>
2025-07-13 14:21:31 +0530

    feat(restaurant-service): add image to both menu and restaurant models

--- backend/restaurant-service/Models/MenuItem.js
+++ backend/restaurant-service/Models/MenuItem.js
@@ -5,9 +5,14 @@ const menuSchema = new mongoose.Schema({
     {
       name: String,
       price: Number,
-      category: String
-    }
-  ]
+      category: String,
+      image: {
+        type: String,
+        default:
+          "https://media.istockphoto.com/id/1829241109/photo/enjoying-a-brunch-together.jpg?s=612x612&w=0&k=20&c=9awLLRMBLeiYsrXrkgzkoscVU_3RoVwl_HA-OT-srjQ=",
+      },
+    },
+  ],
 });
 
 module.exports = mongoose.model("Menu", menuSchema);
